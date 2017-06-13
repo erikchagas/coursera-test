3299997be7112e97cf9b906b94e2b18cc0c98234
Erik <erikchagas00@gmail.com>
2017-06-13 02:30:03 -0400

    Identation corretion on app.js

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,48 +1,47 @@
 (function () {
-'use strict';
+  'use strict';
 
-angular.module('LunchCheck', [])
+  angular.module('LunchCheck', [])
 
-.controller('LunchCheckController', LunchCheckController);
+  .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-  $scope.lunchList = '';
-  $scope.lunchMsg = '';
-  $scope.color = 'black';
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.lunchList = '';
+    $scope.lunchMsg = '';
+    $scope.color = 'black';
 
-  $scope.checkLunch = function () {
-    var totalItems = calculateList($scope.lunchList);
-    if (totalItems === 0){
-      $scope.lunchMsg = 'Please enter data first';
-      setRed();
-    } else if (totalItems > 0 && totalItems <= 3){
-      $scope.lunchMsg = 'Enjoy!';
-      setGreen();     
-    } else {
-      $scope.lunchMsg = 'Too much!';
-      setGreen();
+    $scope.checkLunch = function () {
+      var totalItems = calculateList($scope.lunchList);
+      if (totalItems === 0){
+        $scope.lunchMsg = 'Please enter data first';
+        setRed();
+      } else if (totalItems > 0 && totalItems <= 3){
+        $scope.lunchMsg = 'Enjoy!';
+        setGreen();     
+      } else {
+        $scope.lunchMsg = 'Too much!';
+        setGreen();
+      }
+    };
+
+    function calculateList(str) {
+      var arr = str.split(',');
+
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i].length === 0)
+          arr.splice(i, 1);
+      }
+
+      return arr.length;
     }
-  };
 
-  function calculateList(str) {
-    var arr = str.split(',');
-
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i].length === 0)
-        arr.splice(i, 1);
+    function setRed(){
+      $scope.color = 'red';    
     }
 
-    return arr.length;
+    function setGreen(){
+      $scope.color = 'green';     
+    }  
   }
-
-  function setRed(){
-    $scope.color = 'red';    
-  }
-
-  function setGreen(){
-    $scope.color = 'green';     
-  }  
-}
-
 })();
